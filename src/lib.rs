31f461c32a5c47101d1d57182167dac607395104
Justin Dennison <justin1dennison@gmail.com>
2018-06-24 23:16:11 -0400

    completed the first setup of the netcdf struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,68 @@
 
+use std::fs::{File};
 
-#[derive(Debug)]
-struct NetCDF {
-    filename: String
+#[derive(Debug, Clone)]
+pub enum Dtype {
+    Float32,
+    Float64,
+    Int16,
+    Int32,
+    Int64,
+    Str
+}
+
+#[derive(Debug, Clone)]
+pub struct Variable {
+    name: String,
+    shape: Shape,
+    dimensions: Vec<Dimension>,
+    dtype: Dtype
+}
+#[derive(Debug, Clone)]
+pub struct Dimension {
+    name: String,
+    size: u64,
+    dtype: Dtype
+}
+
+pub type Shape = Vec<u32>;
+
+#[derive(Debug, Clone)]
+pub struct NetCDF {
+    filename: Option<String>,
+    variables: Vec<Variable>,
+    dimensions: Vec<Dimension>
 }
 
 impl NetCDF {
+    pub fn new() -> NetCDF {
+        NetCDF{
+            filename: None,
+            variables: vec![],
+            dimensions: vec![]
+        }  
+    }
     pub fn open(filename: String) -> NetCDF {
-        return NetCDF {
-            filename: filename 
+        let variables = vec![];
+        let dimensions = vec![];
+        NetCDF {
+            filename: Some(filename),
+            variables,
+            dimensions
         } 
+    }
+
+    pub fn add_dimension(&mut self, name: String, size: u64, dtype: Dtype) -> Dimension {
+        let dim = Dimension { name, size, dtype };
+        self.dimensions.push(dim.clone());
+        return dim;
+    }
+
+    pub fn add_variable(&mut self, name: String, dtype: Dtype, dimensions: Vec<Dimension>) -> Variable{
+        let shape = dimensions.iter().map(|r| r.size as u32).collect::<Vec<u32>>();
+        let variable = Variable{ name, dtype, dimensions, shape };
+        self.variables.push(variable.clone());
+        return variable;
     }
 }
 
